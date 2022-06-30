b3b010c829e91f4705bb1fdb311b6ee87a08cb77
maheshmane1234 <107165694+maheshmane1234@users.noreply.github.com>
2022-06-30 11:47:44 +0530

    TSTI-41: Extra spaced Line removed from PrintException

--- PrintException.java
+++ PrintException.java
@@ -6,11 +6,9 @@ public class PrintException {
 			System.out.println(10/0);
 		} catch(ArithmeticException e) {
 			e.printStackTrace();
-			
 			System.out.println(e.toString());
 			System.out.println();
 			System.out.println(e.getMessage());
 		}
 	}
-
 }
