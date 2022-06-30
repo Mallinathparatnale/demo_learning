d8f6ae4ec057022dd05bd943cf4948d469c71c4a
maheshmane1234 <107165694+maheshmane1234@users.noreply.github.com>
2022-06-30 11:53:13 +0530

    TSTI-41: PrintException Code Modified

--- PrintException.java
+++ PrintException.java
@@ -3,6 +3,7 @@ package exceptionProblems;
 public class PrintException {
 	public static void main(String args[]) {
 		try {
+			System.out.println("Exception Hadling Path");
 			System.out.println(10/0);
 		} catch(ArithmeticException e) {
 			e.printStackTrace();
